b73bcfde3f0ffab15ba6c41bf76db1929bbe975b
Vi <vi812x@gmail.com>
2023-08-21 16:12:05 +0300

    Борьба с формой обратной связи

--- library/feedback/views.py
+++ library/feedback/views.py
@@ -1,52 +1,101 @@
 from random import randint
-from django.shortcuts import render, redirect
-from django.core.mail import send_mail
+from django.shortcuts import render
 from django.conf import settings
+from django.core.mail import send_mail
 from .forms import FeedbackForm
+from .models import Feedback
+from books.models import AppSettings
+
+
+def send_feedback(feedback_data):
+    subject = "Сообщение из формы обратной связи"
+
+    message = ""
+    if feedback_data.name:
+        message += f"\nИмя отправителя: {feedback_data.name}"
+    message += f"\nЭлектронная почта: {feedback_data.email}"
+    if feedback_data.phone:
+        message += f"\nТелефон: {feedback_data.phone}"
+    message += f"\nТекст сообщения: {feedback_data.message}"
+
+    from_email = settings.EMAIL_HOST_USER
+
+    app_settings = AppSettings.objects.first()
+    recipient_list = [app_settings.feedback_email]
+
+    send_mail(subject, message, from_email, recipient_list, fail_silently=False)
 
 
 def feedback_view(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         captcha_num1 = randint(0, 20)
         captcha_num2 = randint(0, 20)
         form = FeedbackForm()
         context = {
-            'form': form,
-            'captcha_num1': captcha_num1,
-            'captcha_num2': captcha_num2,
+            "form": form,
+            "captcha_num1": captcha_num1,
+            "captcha_num2": captcha_num2,
         }
         return render(request, 'feedback/feedback_form.html', context)
     else:
-
-
-
-
-
         form = FeedbackForm(request.POST)
-        captcha_num1 = randint(0, 20)
-        captcha_num2 = randint(0, 20)
         if form.is_valid():
-            captcha_answer = request.POST.get('captcha')
-            captcha_solution = str(captcha_num1 + captcha_num2)
 
-            if captcha_answer == captcha_solution:
-                feedback = form.save()
+            form_captcha1 = int(request.POST.get('captcha_num1', 0))
+            form_captcha2 = int(request.POST.get('captcha_num2', 0))
+            captcha = int(request.POST.get('captcha', 1))
 
-                subject = 'New Feedback'
-                message = f"Name: {feedback.name}\nEmail: {feedback.email}\nMessage: {feedback.message}"
-                from_email = settings.DEFAULT_FROM_EMAIL
-                recipient_list = [settings.FEEDBACK_EMAIL]
-                send_mail(subject, message, from_email, recipient_list)
+            if captcha == form_captcha1 + form_captcha2:
+                feedback_data = Feedback(
+                    name=form.cleaned_data['name'],
+                    email=form.cleaned_data['email'],
+                    phone=form.cleaned_data['phone'],
+                    message=form.cleaned_data['message']
+                )
+                feedback_data.save()
 
-                return redirect('success_page')
-            else:
+                send_feedback(feedback_data)
+
+                form = FeedbackForm()
+                captcha_num1 = randint(0, 20)
+                captcha_num2 = randint(0, 20)
                 context = {
-                    'form': form,
-                    'captcha_error': True,
-                    'captcha_num1': captcha_num1,
-                    'captcha_num2': captcha_num2
+                    "form": form,
+                    "captcha_num1": captcha_num1,
+                    "captcha_num2": captcha_num2,
+                    "message_to_user": "Сообщение успешно отправлено!",
+                }
+                return render(request, 'feedback/feedback_form.html', context)
+
+            else:
+                captcha_num1 = randint(0, 20)
+                captcha_num2 = randint(0, 20)
+                context = {
+                    "form": form,
+                    "captcha_num1": captcha_num1,
+                    "captcha_num2": captcha_num2,
+                    "message_to_user": "Ошибка проверки поля captcha! Попробуйте еще раз!",
                 }
                 return render(request, 'feedback/feedback_form.html', context)
         else:
-            context = {'form': form, 'captcha_num1': captcha_num1, 'captcha_num2': captcha_num2}
+            print(form.errors)
+
+            form_data = {
+                'name': request.POST.get('name'),
+                'email': request.POST.get('email'),
+                'phone': request.POST.get('phone'),
+                'message': request.POST.get('message'),
+            }
+
+            print(form_data)
+            form = FeedbackForm(initial=form_data)
+
+            captcha_num1 = randint(0, 20)
+            captcha_num2 = randint(0, 20)
+            context = {
+                "form": form,
+                "captcha_num1": captcha_num1,
+                "captcha_num2": captcha_num2,
+                "message_to_user": "Ошибка заполнения формы! Попробуйте еще раз!",
+            }
             return render(request, 'feedback/feedback_form.html', context)
